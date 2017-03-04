397ffe6bcf479eab90d86a4105740cb1690b39c4
Krister Johansson <Krister Johansson>
2017-03-04 19:20:44 +0100

    First Commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var express = require('express')
+require('dotenv').config()
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on port 3000!')
+})
